c94d671f35333e944c34e1b38737de6cdee2b20c
MiMo <michael.a.morris.public@gmail.com>
2019-08-15 17:46:29 -0500

    Create static version of multiple list display

---
+++ src/components/TodoListSummary/TodoListSummary.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import css from './TodoListSummary.module.css'
+import DeleteIcon from '../DeleteIcon/DeleteIcon'
+
+const TodoListSummary = ({ summary }) => {
+  return (
+    <div className={css.TodoListSummary}>
+      <h3 className={css.header}>{summary.name}</h3>
+      <div className={css.content}>
+        <div className={css.content__counts}>
+          <p className={css.content__countsText}>
+            Finished:
+            <span className={css.content__countsDigit}>
+              <strong>{summary.finished}</strong>
+            </span>
+          </p>
+          <p className={css.content__countsText}>
+            Unfinished:
+            <span className={css.content__countsDigit}>
+              <strong>{summary.unfinished}</strong>
+            </span>
+          </p>
+        </div>
+        <DeleteIcon />
+      </div>
+    </div>
+  )
+}
+
+export default TodoListSummary
--- src/components/TodoLists/TodoLists.jsx
+++ src/components/TodoLists/TodoLists.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 import css from './TodoLists.module.css'
+import TodoListSummary from '../TodoListSummary/TodoListSummary'
+import InputBox from '../InputBox/InputBox'
 
 const fakeData = [
   { name: 'General Todos', finished: '2', unfinished: '3' },
@@ -10,10 +12,32 @@ const fakeData = [
 
 export default class TodoLists extends Component {
   state = {
-    lists: []
+    lists: fakeData
+  }
+
+  renderChildren = () => {
+    return this.state.lists.map((summary, idx) => (
+      <TodoListSummary summary={summary} key={idx} />
+    ))
+  }
+
+  renderListCreator = () => {
+    return (
+      <div className={css.addButtonContent}>
+        <i className='material-icons'>playlist_add</i>
+        <span style={{ display: 'inline' }}>Add List</span>
+      </div>
+    )
   }
 
   render () {
-    return <div className={css.TodoLists}>Data</div>
+    return (
+      <div className={css.TodoLists}>
+        <div className={css.TodoLists__addSection}>
+          <InputBox>{this.renderListCreator()}</InputBox>
+        </div>
+        {this.renderChildren()}
+      </div>
+    )
   }
 }
